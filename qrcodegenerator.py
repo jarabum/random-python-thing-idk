98d20438619bb22173ac5d8f234710d8a67b569e
jarabum <jara11.str@gmail.com>
2024-12-10 16:34:34 +0100

    upraveno qrcodegenerator.py

--- qrcodegenerator.py
+++ qrcodegenerator.py
@@ -1,5 +1,6 @@
 import qrcode
 import time 
+import os
 
 url = input("zadej url nebo text do qr kodu: ")
 img = qrcode.make(url)
@@ -7,4 +8,11 @@ img = qrcode.make(url)
 jmeno = input("zadej jmeno qrkodu: ")
 img.save(jmeno + ".png")
 print("hotovo")
-input("napis sem neco pro konec: ")
+moznost = input("chcete smazat qr kod (Y/N): ")
+if moznost == "Y" or moznost == "y":
+    os.remove(jmeno + ".png")
+    print("smazano")
+    time.sleep(3)
+else:
+    print("sbohem")
+    time.sleep(3)
